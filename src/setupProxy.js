ba37e52d1dbec76dd501db6f7de9da5132fa4d07
Danny Rangel <danielrangel.tech@gmail.com>
2019-11-30 19:50:01 -0800

    prod changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,14 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
+    // app.use(
+    //     proxy(['/api', '/auth/google', '/api/images'], {
+    //         target: 'http://localhost:8081'
+    //     })
+    // );
     app.use(
         proxy(['/api', '/auth/google', '/api/images'], {
-            target: 'http://localhost:8081'
+            target: 'http://clouddrop.us-west-1.elasticbeanstalk.com'
         })
     );
 };
